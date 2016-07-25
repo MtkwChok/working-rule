ee79f9dcf397285cfd7f17a7aab7ffee2dfc7c55
chok <chokm58@gmail.com>
2016-07-25 20:34:18 +0000

    2nd base

--- working-rule/app/controllers/users_controller.rb
+++ working-rule/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @user = User.page(params[:page]).per(10)
+  end
 
   def show
     @user = User.find(params[:id])
@@ -18,10 +24,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Success! It has been changed."
+      redirect_to @user      
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_url
+    end
   end
 
 end
